1c9710f8d11ec9360212e1fc1d5483ef65ada5d8
Paul Esperon <paul.esperonarg@hotmail.com>
2025-03-11 00:21:53 -0300

    Add dark mode functionality and improve the search bar in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./DarkMode";
 
 const MenuLinks = [
   { id: 1, title: "Home", link: "/#" },
@@ -42,13 +44,21 @@ const Navbar = () => {
           {/* Navbar Rigth section  */}
           <div className="flex justiy-between items-center gap-4">
             {/* Search Bar secion  */}
-            <div className="realative group hidden sm:block">
+            <div className="relative group hidden sm:block">
               <input type="text" placeholder="Search" className="search-bar" />
-              <IoMdSearch className="text-xl text-gray-600 dark:text-gray-400" />
+              <IoMdSearch className="text-xl text-gray-600 group-hover:text-secondary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200" />
             </div>
 
+            {/* Cart Icon secion  */}
+            <button className="relative p-3">
+              <FaCartShopping className="text-xl text-gray-600dark:text-gray-400"/>
+              <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs">4</div>
+            </button>
+
             {/* Dark mode secion  */}
-            <div></div>
+            <div>
+                <DarkMode />
+            </div>
           </div>
         </div>
       </div>
