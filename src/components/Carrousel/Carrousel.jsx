0cd70c0b8e1b5651aa5bb90b075dde3ef6c34dbc
Paul Esperon <paul.esperonarg@hotmail.com>
2025-03-14 22:07:20 -0300

    Improve the structure of the Carrousel component and adjust presentation styles.

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -41,9 +41,9 @@ const Carrousel = () => {
     infinite: true,
     speed: 800,
     slidesToScroll: 1,
-    //autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 4000,
-    cssEase: "aese-in-out",
+    cssEase: "ease-in-out",
     pauseOnHover: false,
     pauseOnFocus: true,
   };
@@ -51,34 +51,35 @@ const Carrousel = () => {
   return (
     <div className="container">
       <div className="overflow-hidden rounded-3xl min-h-[550px] sm:min-h-[650px] carrousel-bg-color flex justify-center items-center">
-
         <div className="container pb-8 sm:pb-0">
-        
-        {/* Carrousel Section  */}
-        <Slider {...settings}>
-          {imagesData.map((image) => (
-            <div key={image.id}>
-              <div className="grid grid-cols-1 sm:grid-cols-2">
-                {/* text content section  */}
-                <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1">
+          {/* Carrousel Section  */}
+          <Slider {...settings}>
+            {imagesData.map((image) => (
+              <div key={image.id}>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                  {/* text content section  */}
+                  <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
                     <h1>{image.subtitulo}</h1>
                     <h1>{image.title}</h1>
                     <h1>{image.title2}</h1>
                     <div>
-                        <button>Shop Now</button>
+                      <button>Shop Now</button>
                     </div>
-                    
-                {/* Img Section  */}
-                <div className="order-1 sm:order-2">
+                  </div>
+                  {/* Img Section  */}
+                  <div className="order-1 sm:order-2">
                     <div>
-                        <img src={image.img} alt="" className="w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]"/>
+                      <img
+                        src={image.img}
+                        alt=""
+                        className="w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)]"
+                      />
                     </div>
-                </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
         </div>
       </div>
     </div>
