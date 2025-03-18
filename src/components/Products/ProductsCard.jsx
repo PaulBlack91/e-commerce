39255a1b143d5e07c36d799accc6df515cc0fe80
Paul Esperon <paul.esperonarg@hotmail.com>
2025-03-17 21:07:51 -0300

    Add "Add to Cart" button on ProductsCard component with hover effect

--- src/components/Products/ProductsCard.jsx
+++ src/components/Products/ProductsCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../Shared/Button";
 
 const ProductsCard = ({ data }) => {
   return (
@@ -7,11 +8,23 @@ const ProductsCard = ({ data }) => {
         {/* card section */}
 
         {data.map((data) => (
-          <div key={data.id}>
+          <div className="group" key={data.id}>
             <div className="relative">
-              <img src={data.image} alt="" className=" bg-secondary [h-180px] w-[260px] object-cover rounded-md" />
+              <img
+                src={data.image}
+                alt=""
+                className=" bg-secondary [h-180px] w-[260px] object-cover rounded-md"
+              />
 
-              
+              {/* hover button  */}
+
+              <div className="hidden group-hover:flex absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 h-full w-full text-center group-hover:backdrop-blur-sm justify-center items-center duration-200 rounded-md">
+                <Button
+                  text="Add to Cart"
+                  bgColor="bg-primary"
+                  textColor="text-black"
+                />
+              </div>
             </div>
             <div className="leading-7">
               <h2 className="font-bold">{data.title}</h2>
