6fcc6577561085a401567a76211bc69ff593c29a
Paul Esperon <paul.esperonarg@hotmail.com>
2025-03-20 23:18:47 -0300

    Add Blogs component and integrate it into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Carrousel from './components/Carrousel/Carrousel'
 import Banner from './components/Banner/Banner'
 import Products from './components/Products/Products'
+import Blogs from './components/Blogs/Blogs'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Banner />
       <Carrousel />
       <Products />
+      <Blogs/>
     </div>
   )
 }
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Heading from '../Shared/Header.jsx'
+
+const Blogs = () => {
+  return (
+    <div>
+      <div className="container">
+        {/* Header section   */}
+        <Heading title="Recent News" subtitle={"Explore Our Blogs"}/>
+
+        {/* Blog section   */}
+
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Blogs; 
